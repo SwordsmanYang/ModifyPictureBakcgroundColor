7565e6a1a145ef3b3476c9d12f60062123b71fbf
yangcq <yangcq@yangcqdeMacBook-Pro.local>
2017-07-26 19:32:00 +0800

    Initial Commit

---
+++ ModifyPictureBakcgroundColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModifyPictureBakcgroundColor
+//
+//  Created by yangcq on 2017/7/26.
+//  Copyright © 2017年 yangcq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModifyPictureBakcgroundColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModifyPictureBakcgroundColor
+//
+//  Created by yangcq on 2017/7/26.
+//  Copyright © 2017年 yangcq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
