9cf775622a3af7a21dc8915de6d98fddf81097b1
FangDangZi <1392550437@qq.com>
2017-07-28 15:29:28 +0800

    2
    第二次提交

--- ModifyPictureBakcgroundColor/ViewController.m
+++ ModifyPictureBakcgroundColor/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
@@ -19,6 +21,55 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self drawImage];
+}
+
+- (void)drawImage
+{
+    CGImageRef cgimage = [self.imageView.image CGImage];
+    
+    size_t width = CGImageGetWidth(cgimage); // 图片宽度
+    size_t height = CGImageGetHeight(cgimage); // 图片高度
+    unsigned char *data = calloc(width * height * 4, sizeof(unsigned char)); // 取图片首地址
+    size_t bitsPerComponent = 8; // r g b a 每个component bits数目
+    size_t bytesPerRow = width * 4; // 一张图片每行字节数目 (每个像素点包含r g b a 四个字节)
+    CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB(); // 创建rgb颜色空间
+    
+    CGContextRef context =
+    CGBitmapContextCreate(data,
+                          width,
+                          height,
+                          bitsPerComponent,
+                          bytesPerRow,
+                          space,
+                          kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), cgimage);
+    
+    for (size_t i = 10; i < height; i++)
+    {
+        for (size_t j = 10; j < width; j++)
+        {
+            size_t pixelIndex = i * width * 4 + j * 4;
+            
+            unsigned char red = data[pixelIndex];
+            unsigned char green = data[pixelIndex + 1];
+            unsigned char blue = data[pixelIndex + 2];
+            
+            //过滤代码
+            if ((red < 0x2f && red> 0x07) && (green < 0xa0 && green> 0x84) && (blue < 0xbf && blue> 0xa8)) {
+                //设置成白色
+                data[pixelIndex] = 255;
+                data[pixelIndex + 1] = 255;
+                data[pixelIndex + 2] = 255;
+            }
+        }
+    }
+    
+    cgimage = CGBitmapContextCreateImage(context);
+    self.imageView.image = [UIImage imageWithCGImage:cgimage];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
